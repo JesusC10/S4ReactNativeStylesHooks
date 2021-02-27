b28552d8a62c6bee7e756373b7ddcca78115cac3
David Iturriaga Sotelo <e-diturriaga@neoris.com>
2021-02-27 07:56:17 -0800

    Compose method.

--- App.js
+++ App.js
@@ -4,18 +4,30 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const page = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 40,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: '#000'
   },
 });
+
+const lists = StyleSheet.create({
+  listContainer: {
+    flex: 1,
+    backgroundColor: '#61dafb',
+  },
+});
+
+const container = StyleSheet.compose(page.container, lists.listContainer);
